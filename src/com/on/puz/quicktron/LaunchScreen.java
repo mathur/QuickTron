984b3c5843611e3e511b022308c4fcb370a32c2f
Joe Doyle (Ginto8) <ginto8@gmail.com>
2013-10-12 17:11:46 -0400

    Integrate hulling w/ UI

--- src/com/on/puz/quicktron/LaunchScreen.java
+++ src/com/on/puz/quicktron/LaunchScreen.java
@@ -12,6 +12,9 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.on.puz.quicktron.R;
+
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
