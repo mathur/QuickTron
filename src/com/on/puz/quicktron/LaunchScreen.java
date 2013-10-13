39d55f342d0ae0a239529860e1d1c36ba6c7687e
Liezl <liezl.200@gmail.com>
2013-10-13 00:18:27 -0400

    New Test Title Prompt

--- src/com/on/puz/quicktron/LaunchScreen.java
+++ src/com/on/puz/quicktron/LaunchScreen.java
@@ -1,17 +1,38 @@
 package com.on.puz.quicktron;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LaunchScreen extends Activity {
 
 
 	public void scanNewTest(View view)
 	{
-		Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+		final EditText input = new EditText(this);
+		final AlertDialog.Builder newName = new AlertDialog.Builder(this);
+		newName.setTitle("New Test Name");
+		newName.setView(input);
+		AlertDialog alert = newName.create();
+		alert.setButton(DialogInterface.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+	        public void onClick(final DialogInterface dialog, final int whichButton) {
+	            final String value = input.getText().toString().trim();
+	    		Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+	    		startActivity(intent);
+	        }
+	    });
+
+		alert.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int whichButton) {
+	            dialog.cancel();
+	        }
+	    });
+		alert.show();
+	
 
 	}
 	public void viewTestsList(View view)
