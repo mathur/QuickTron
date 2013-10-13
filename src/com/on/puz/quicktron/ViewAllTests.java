5241a3580480ba38b2d79ae84076615ed6eebe0f
Rohan Mathur <rohanmathur34@gmail.com>
2013-10-13 04:40:15 -0400

    Remove unused menu button/menu settings from ViewTests
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- src/com/on/puz/quicktron/ViewAllTests.java
+++ src/com/on/puz/quicktron/ViewAllTests.java
@@ -8,7 +8,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -294,13 +293,4 @@ private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
 	    });
 		alert.show();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.view_all_tests, menu);
-		return true;
-	}
-	
-
 }
